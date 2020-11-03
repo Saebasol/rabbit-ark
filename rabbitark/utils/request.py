6b006b5a211324dd2ee6fdf02af8e9738571eb8b
Ryu JuHeon <saidbysolo@gmail.com>
2020-11-03 17:13:19 +0900

    refactor(utils/request): edit headers

--- rabbitark/utils/request.py
+++ rabbitark/utils/request.py
@@ -6,14 +6,10 @@ class Requester:
     def __init__(
         self,
         user_agent: Optional[str] = None,
-        range: Optional[str] = None,
         referer: Optional[str] = None,
-        origin: Optional[str] = None,
     ):
         self.referer = referer
         self.user_agent = user_agent
-        self.range = range
-        self.origin = origin
 
     @property
     def headers(self):
@@ -22,19 +18,21 @@ class Requester:
             headers.update({"referer": self.referer})
         if self.user_agent:
             headers.update({"User-Agent": self.user_agent})
-        if self.range:
-            headers.update({"Range": self.range})
-        if self.origin:
-            headers.update({"origin": self.range})
         return headers
 
     async def request(
-        self, url: str, method: str, response_method: str, json: Any = None
+        self,
+        url: str,
+        method: str,
+        response_method: str,
+        headers: Any = None,
+        json: Any = None,
     ) -> Any:
         async with aiohttp.ClientSession(headers=self.headers) as cs:
             async with cs.request(
                 method,
                 url,
+                headers=headers,
                 json=json,
             ) as response:
                 dispatch = {
@@ -48,10 +46,14 @@ class Requester:
                     )
                 return await dispatch[response_method]()
 
-    async def get(self, url: str, response_method: str = "read") -> Any:
+    async def get(
+        self, url: str, response_method: str = "read", headers: Any = None
+    ) -> Any:
         """Perform HTTP GET request."""
-        return await self.request(url, "GET", response_method)
+        return await self.request(url, "GET", response_method, headers)
 
-    async def post(self, url: str, response_method: str, json: Any = None) -> Any:
+    async def post(
+        self, url: str, response_method: str, json: Any = None, headers: Any = None
+    ) -> Any:
         """Perform HTTP POST request."""
-        return await self.request(url, "POST", response_method, json)
+        return await self.request(url, "POST", response_method, json, headers)
