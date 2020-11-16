a4b33b426305cb4a4ad8586e88329f5bbfbb8c5d
Ryu JuHeon <saidbysolo@gmail.com>
2020-11-17 00:00:30 +0900

    style(utils): change class name

--- rabbitark/__main__.py
+++ rabbitark/__main__.py
@@ -6,13 +6,14 @@ import sys
 from rabbitark.config import config
 from rabbitark.rabbitark import RabbitArk
 from rabbitark.utils.utils import load_cookie_txt
+from rabbitark.extractor import load
 
 if getattr(sys, "frozen", False):
     multiprocessing.freeze_support()
 
 parser: argparse.ArgumentParser = argparse.ArgumentParser("rabbitark")
 
-parser.add_argument("extractor", type=str, help="Specifies the extractor to use")
+parser.add_argument("extractor", type=str, help="Specifies the extractor name")
 
 parser.add_argument(
     "--downloadable",
@@ -22,11 +23,13 @@ parser.add_argument(
 
 parser.add_argument("--base", type=str, help="Specifies the pre-created folder")
 
-parser.add_argument("--folder", type=str, help="")
+parser.add_argument("--folder", type=str, help="Specifies the folder name")
 
 parser.add_argument("--cookies", type=str, help="load cookies.txt")
 
 parser.add_argument("--page", type=int, help="Youtube page limit")
+
+parser.add_argument("--custom_extractor", type=str, help="use custom extractor")
 
 args: argparse.Namespace = parser.parse_args()
 
@@ -42,4 +45,8 @@ if args.cookies:
 if args.page:
     config.YOUTUBE_PAGE_LIMIT = args.page
 
+if args.custom_extractor:
+    config.CUSTOM_EXTRACTOR = args.custom_extractor
+
+load()
 asyncio.run(RabbitArk(args.extractor).start(args.downloadable))
