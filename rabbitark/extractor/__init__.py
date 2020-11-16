c04973efe8caca9d038090f78f94fabc2b2bcf0e
Ryu JuHeon <saidbysolo@gmail.com>
2020-11-17 00:02:42 +0900

    feat(extractor): import all extractor

--- rabbitark/extractor/__init__.py
+++ rabbitark/extractor/__init__.py
@@ -1,3 +1,20 @@
-from .hitomi import Hitomi
-from .pixiv import Pixiv
-from .youtube import Youtube
+import os
+import re
+import traceback
+
+directory = os.path.dirname(os.path.realpath(__file__))
+
+
+def load():
+    failed = []
+
+    for extension in [
+        re.sub(".py", "", file) for file in os.listdir(directory) if not "__" in file
+    ]:
+        try:
+            __import__("rabbitark.extractor." + extension)
+        except:
+            traceback.print_exc()
+            failed.append(extension)
+
+    return failed
