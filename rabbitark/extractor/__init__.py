bf9e5a4ffa7d51ac89103cac2ef488307a8b9098
Ryu JuHeon <saidbysolo@gmail.com>
2020-11-18 22:28:17 +0900

    fix(extractor): pyinstaller extractor load issue

--- rabbitark/extractor/__init__.py
+++ rabbitark/extractor/__init__.py
@@ -2,18 +2,29 @@ import os
 import re
 import traceback
 from importlib import import_module
+import sys
 
-directory = os.path.dirname(os.path.realpath(__file__))
+if getattr(sys, "frozen", False):
+    directory = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
+else:
+    directory = os.path.dirname(os.path.realpath(__file__))
 
 
 def load():
     failed = []
 
     for extension in [
-        re.sub(".py", "", file) for file in os.listdir(directory) if not "__" in file
+        re.sub(".py", "", file)
+        for file in os.listdir(directory)
+        if not "__" in file
+        if os.path.splitext(file)[1] == ".py"
     ]:
         try:
-            import_module("rabbitark.extractor." + extension)
+            import_module(
+                extension
+                if getattr(sys, "frozen", False)
+                else f"rabbitark.extractor.{extension}"
+            )
         except:
             traceback.print_exc()
             failed.append(extension)
