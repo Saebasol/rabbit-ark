d2b9d77987f06dd0e38a1266d1f79246860d7215
Ryu JuHeon <saidbysolo@gmail.com>
2020-11-22 19:48:57 +0900

    feat(rabbitark): add version info and author

---
+++ rabbitark/__init__.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+import sys
+
+__version__ = "0.1.0"
+
+__author__ = "Ryu JuHeon"
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
